4e73f86c978568666c8773178b510c47a2b14614
Manuel Adan <m_adan2012@outlook.com>
2022-10-18 16:56:11 -0500

    button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import {Navbar} from './components/Navbar';
 import './styles/styles.scss';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
+import { AiFillAlert, AiFillCarryOut } from "react-icons/ai";
+import Button from './components/Button';
 
 function App() {
      return (
           <div className="App">
-               <Navbar />
+               <Navbar /> 
+               <Button> <AiFillAlert /> Texto  </Button>
+               <Button> <AiFillCarryOut /> Soy otro botton </Button>
+                              
+               <AiFillCarryOut />
                <Banner promocion="ÚNETE AL CLUB Y CONSIGUE UN 15% DE DESCUENTO" boton="registrate" onClick={() => console.log("click en registrate")}/>
                <main className="content">
-                    <AppContainer />
+                   {/*  <AppContainer /> */}
                </main>
                <Banner promocion="ÚNETE AL CLUB Y CONSIGUE UN 35% DE DESCUENTO" boton="unete" onClick={() => console.log("click en unete")}/>
                <Footer />
